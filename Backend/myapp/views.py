a80d2032eb4af85c7d6465452c557485ff83beff
nbhdqxt <ubuntu@ip-172-31-22-75.ap-northeast-2.compute.internal>
2025-02-10 15:21:43 +0000

    fix: Fix error in save_medicert
    #9

--- Backend/myapp/views.py
+++ Backend/myapp/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.http import JsonResponse
 from dotenv import load_dotenv
 from Utils.vas import vas_cal
@@ -6,7 +5,9 @@ from Utils.presum import get_presum
 from Utils.medicert import save_medicert
 from Utils.gpt import agent_response
 import asyncio
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 load_dotenv(dotenv_path="myapp/.env")
 # Create your views here.
@@ -19,9 +20,14 @@ def presum_response(request, input_string):
 	response = get_presum(input_string)
 	return JsonResponse({'presum' : str(response)})
 
+@api_view(['POST'])
 def medicert_save(request):
-	response = save_medicert(request)
-	return response
+    data = request.data
+    try:
+        save_medicert(data)
+        return Response({"message": "데이터 저장 성공"}, status=status.HTTP_201_CREATED)
+    except Exception as e:
+        return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 def chatbot_response(request, input_string):
     """Django 뷰 함수 - 사용자의 질문을 받고 응답을 반환"""
